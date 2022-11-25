2563c1cf1d419039ff3b0ca9470dabf0f7931679
Kit <simkatesi@yandex.ru>
2022-11-25 15:35:11 +0400

    функция подсчета длинны второго массива

--- Program.cs
+++ Program.cs
@@ -1,9 +1,34 @@
-﻿//создание функции заполнения массива из строки
-// string[] MasString(string [] txt,int m)
-// {
-//     string [] array = new string [m];
-    
-// }
+﻿//создание функции заполнения итогового массива 
+string[] MasString(string [] txt,int m, int n)
+{
+    int j = 0;
+    string [] array = new string [LengthMassiv(txt,n)];
+    for (int i = 0; i < txt.Length; i++)
+    {
+        // if (txt[i]<= n)
+        // {    
+        // array[j]=txt[i];
+        // }
+        // j++;
+    }
+    return array;
+}
+
+int LengthMassiv(string[] arr, int n) //ф-ия подсчета элементов массива с <=3 символами
+{
+    int count = 0; 
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= n) 
+        {
+            Console.Write( $" {arr[i]} , ");
+            count++; 
+        }
+    }
+    return count; 
+}
+
 
 void PrintArry(string[] massiv)
 {
@@ -23,10 +48,11 @@ void PrintArry(string[] massiv)
 Console.Clear();
 Console.Write("Введите слова через пробел: ");
 string? text = Console.ReadLine();
-//int predel = 3;
-//int length = text.Length;
-string[] text1 = text.Split(" ");
+int predel = 3;
+string [] text1 = text.Split(" "); //массив из строки
+int length = text1.Length;
 PrintArry (text1);
-// string[] text2 = MasString(text1,length);
-// PrintArry (text2);
+
+string[] text2 = MasString(text1,length,predel);
+PrintArry (text2);
 
