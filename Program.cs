1e9bf38976494756a6083070e8cb63579545ec43
Kit <simkatesi@yandex.ru>
2022-11-25 16:37:12 +0400

    массив из выбранных с определенными параметрами  слов

--- Program.cs
+++ Program.cs
@@ -1,43 +1,43 @@
 ﻿//создание функции заполнения итогового массива 
-string[] MasString(string [] txt,int m, int n)
+string[] MasString(string[] txt, int n)
 {
+    string[] array = new string[LengthMassiv(txt, n)];
     int j = 0;
-    string [] array = new string [LengthMassiv(txt,n)];
     for (int i = 0; i < txt.Length; i++)
     {
-        // if (txt[i]<= n)
-        // {    
-        // array[j]=txt[i];
-        // }
-        // j++;
+        if (txt[i].Length <= n)
+        {
+            array[j] = txt[i];
+            j++;
+        }
     }
     return array;
 }
 
 int LengthMassiv(string[] arr, int n) //ф-ия подсчета элементов массива с <=3 символами
 {
-    int count = 0; 
+    int count = 0;
 
     for (int i = 0; i < arr.Length; i++)
     {
-        if (arr[i].Length <= n) 
+        if (arr[i].Length <= n)
         {
-            Console.Write( $" {arr[i]} , ");
-            count++; 
+            count++;
+            //        Console.Write($" {count} ");
         }
     }
-    return count; 
+    return count;
 }
 
-
+//функция печати
 void PrintArry(string[] massiv)
 {
     int count = massiv.Length;
     Console.Write("[");
-    for (int i = 0;i < count;i++)
+    for (int i = 0; i < count; i++)
     {
         Console.Write(massiv[i]);
-        if (i < count-1)
+        if (i < count - 1)
         {
             Console.Write(",");
         }
@@ -49,10 +49,9 @@ Console.Clear();
 Console.Write("Введите слова через пробел: ");
 string? text = Console.ReadLine();
 int predel = 3;
-string [] text1 = text.Split(" "); //массив из строки
-int length = text1.Length;
-PrintArry (text1);
-
-string[] text2 = MasString(text1,length,predel);
-PrintArry (text2);
+string[] text1 = text.Split(" "); //массив из строки
+PrintArry(text1);
+Console.WriteLine();
+string[] text2 = MasString(text1, predel);
+PrintArry(text2);
 
