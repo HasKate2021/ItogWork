e9a59557ce73c8496a1945da1d9ac20342cefd71
Kit <simkatesi@yandex.ru>
2022-11-25 14:38:52 +0400

    функция для печати

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿//создание функции заполнения массива из строки
+// string[] MasString(string [] txt,int m)
+// {
+//     string [] array = new string [m];
+    
+// }
+
+void PrintArry(string[] massiv)
+{
+    int count = massiv.Length;
+    Console.Write("[");
+    for (int i = 0;i < count;i++)
+    {
+        Console.Write(massiv[i]);
+        if (i < count-1)
+        {
+            Console.Write(",");
+        }
+    }
+    Console.Write("]");
+}
+
+Console.Clear();
+Console.Write("Введите слова через пробел: ");
+string? text = Console.ReadLine();
+//int predel = 3;
+//int length = text.Length;
+string[] text1 = text.Split(" ");
+PrintArry (text1);
+// string[] text2 = MasString(text1,length);
+// PrintArry (text2);
+
